6ece57f04921aa63ba3896158dc228655ec6abe9
Killian <killian.couraillon@hotmail.fr>
2021-06-29 20:29:20 +0200

    skeleton of modules and crewmates

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('cocktail', './assets/cocktail.js')
     .addEntry('advice', './assets/advice.js')
+    .addEntry('linkModules', './assets/linkModules.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
