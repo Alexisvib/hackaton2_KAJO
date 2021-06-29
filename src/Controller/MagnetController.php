4be0cf792b3741f799fc55ae8465bebee9928fb5
Alexisvib <alexis.vib@gmail.com>
2021-06-29 10:11:01 +0200

    beginning to do a feature of magnet

---
+++ src/Controller/MagnetController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MagnetController extends AbstractController
+{
+    /**
+     * @Route("/machine", name="machine")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('magnet/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('cocktail', './assets/cocktail.js')
     .addEntry('advice', './assets/advice.js')
+    .addEntry('magnet', './assets/magnet.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
