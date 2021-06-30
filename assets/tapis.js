fb350151fa1e4bb536b76cb3c61b837dfdf78948
banon jonathan <j.banon01@gmail.com>
2021-06-30 09:26:20 +0200

    anim

--- assets/tapis.js
+++ assets/tapis.js
@@ -2,11 +2,26 @@ roue1 = document.getElementById('roue1');
 roue2 = document.getElementById('roue2');
 arrowR = document.getElementById('arrowR');
 rocket  = document.getElementById('rocket')
+buttonFire = document.getElementById('buttonFire');
 
-arrowR.addEventListener('click', (e) => {
+arrowR.addEventListener('click', async e => {
    e.preventDefault();
    roue1.classList.toggle('roueOn')
    roue2.classList.toggle('roueOn')
    rocket.classList.toggle('moveRocket')
    arrowR.classList.toggle('dNone')
+   await sleep(3000)
+   buttonFire.classList.toggle('dNone')
+   buttonFire.classList.toggle('fadeIn')
+
 });
+
+function sleep(ms)
+{
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+buttonFire.addEventListener('click', async e => {
+   
+})
